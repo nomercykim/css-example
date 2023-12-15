e10ffe4afd794a3a7551e0ce56cae7ad3f659067
nomercykim <id6word@gmail.com>
2023-12-15 23:06:40 +0900

    추가

---
+++ src/components/TailwindComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function TailwindComponent() {
+    return <div>
+        <h1 className='text-8xl'> 안녕</h1>
+        <button className='bg-blue-200 rounded-xl px-2'>Button</button>
+    </div>;
+}
+
